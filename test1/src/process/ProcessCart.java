5626b15023added4e0302324f334610f2d81c447
anf <anf@anferneede-mini>
2021-12-14 17:17:20 +0000

    test git

---
+++ test1/src/process/ProcessCart.java
@@ -0,0 +1,28 @@
+package process;
+
+import cart.Cart;
+
+public class ProcessCart {
+
+	private Cart myCart;
+	public Cart getMyCart() {
+		return myCart;
+	}
+	public void setMyCart(Cart myCart) {
+		this.myCart = myCart;
+	}
+	
+	public ProcessCart(Cart inputCart) {
+		this.setMyCart(inputCart);
+	}
+	
+	
+	public void processCart() {
+		
+		
+		
+	}
+
+	
+	
+}
